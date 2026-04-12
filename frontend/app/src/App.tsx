19c92de5c6f022991304507c148e45d8aec35973
fujifilm <2670696747@qq.com>
2026-04-12 20:40:31 +0800

    添加动画库，补充顶部的消除动作，以及消除出场动画

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -17,12 +17,24 @@ import Notice from './sections/Notice';
 import MemberPublicProfile from './sections/MemberPublicProfile';
 import ErrorBoundary from './components/ErrorBoundary';
 import { Toaster } from 'react-hot-toast';
+import { X } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export const queryClient = new QueryClient();
 
-// 应用内容组件（在 TokenProvider 内部）
 function AppContent() {
   const { isLoading } = useToken();
+  const [navbarClicked, setNavbarClicked] = useState(localStorage.getItem("navbarClicked") === "true");
+  useEffect (() => {
+    if (!navbarClicked) {
+      const timer = setTimeout(() => {
+        localStorage.setItem("navbarClicked", "true");
+        setNavbarClicked(true);
+      }, 10000); // 10秒后自动设置为已点击
+      return () => clearTimeout(timer);
+    }
+  }, [])
 
   // 初始化加载时显示加载指示器
   if (isLoading) {
@@ -36,14 +48,34 @@ function AppContent() {
     );
   }
 
+  function handleClick(){
+    localStorage.setItem("navbarClicked","true")
+    setNavbarClicked(true);
+  }
+
+
+
   return (
     <Router>
       <div className="app-container">
-        <div className="h-[50px] flex items-center justify-center bg-slate-300">
-          <p className="text-center text-sm">
-            Welcome to join us and become our exclusive photographer！
-          </p>
-        </div>
+        <AnimatePresence>
+          {(!navbarClicked) && (
+            <motion.div
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              className="h-[50px] flex items-center justify-center bg-slate-300 relative"
+            >
+              <p className="text-center text-sm underline decoration-sky-500 decoration-2 underline-offset-4 decoration-dashed">
+                Welcome to join us and become our exclusive photographer！
+              </p>
+              <button onClick={handleClick} className="absolute right-4">
+                <X className="h-4 w-4 " />
+              </button>
+            </motion.div>
+          )
+        }
+        </AnimatePresence>
         <Navbar />
         <main className="flex-1">
           <Routes>
